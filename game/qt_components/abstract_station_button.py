7600b24f56950b7c520e4255265f6892072f5c90
Jakub Krško <jakubkrs@outlook.sk>
2024-05-14 10:41:34 +0200

    add context menu to station button

--- game/qt_components/abstract_station_button.py
+++ game/qt_components/abstract_station_button.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QPushButton
+from PyQt5.QtWidgets import QWidget, QPushButton, QMenu
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QIcon
 
@@ -9,6 +9,13 @@ class AbstractStationButton(QWidget):
         self.setGeometry(0, 0, 80, 55)
         self.right_click_function = lambda: None
         self.middle_click_function = lambda: None
+
+        self.context_menu = QMenu(self) # move top menu into station button ???
+        self.client_menu = QMenu(self)
+        self.client_menu.setTitle("Klient")
+        self.client_menu.addAction("Action 1")
+        self.context_menu.addMenu(self.client_menu)
+        self.context_menu.addAction("Action 2")
 
         self.button = QPushButton(self)
         icon = QIcon("assets/station_button.png")
@@ -32,3 +39,6 @@ class AbstractStationButton(QWidget):
             self.right_click_function()
         if QMouseEvent.button() == Qt.MiddleButton:
             self.middle_click_function()
+
+    def contextMenuEvent(self, event):
+        self.context_menu.exec_(event.globalPos())
