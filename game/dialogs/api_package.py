c6d5f9d631b85639df3af554e42222748ac0baf8
Jakub Krško <jakubkrs@outlook.sk>
2024-06-15 17:09:10 +0200

    remove connect dialog

--- game/dialogs/api_package.py
+++ game/dialogs/api_package.py
@@ -1,4 +1,3 @@
-from .connect_dialog import ConnectDialog
 from .shunting_dialog import ShuntingDialog
 from .new_train_dialog import NewTrainDialog
 from .train_number_dialog import TrainNumberDialog
